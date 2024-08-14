ae7ec6ba3c6114d3ccc5865d3bc8af60e894f800
sbcodes90 <sydneycburrell29@gmail.com>
2024-08-13 22:04:45 -0400

    css updates

--- my-app/src/UxPage.js
+++ my-app/src/UxPage.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import projectsHeader from "../src/images/projects.png"
+import hhg from "../src/images/hhg.png"
+import hhg2 from "../src/images/hhg2.png"
 
 export default function UxPage() {
   return (
     <div className="lg:p-20">
     <div>
-      <header className="lg:text-right text-5xl tracking-widest	lg:pb-10 text-center pt-10 bg-white	">UX DESIGN</header>
-      <img src={projectsHeader} alt="header-for-projects" className='w-60 pb-7 ml-7'/>
-      <section className="flex flex-col gap-5 lg:flex-row justify-around items-start lg:gap-5 pb-10">
+      <header className="lg:text-right text-5xl tracking-widest	lg:pb-10 text-center bg-white mt-10">UX DESIGN</header>
+      <img src={projectsHeader} alt="header-for-projects" className='w-40 pt-10 lg:w-52 lg:pb-7 ml-10'/>
+      <section className="flex flex-col-reverse  lg:flex-row justify-around items-start  border-black border-4 lg:p-20 border-solid rounded-3xl m-10">
 
-        <div className=" px-10 lg:w-6/12"><div className='text-xl font-bold pb-2'>MentorMeCollective</div> MentorMeCollective is a non-profit organization focused on providing opportunites for first generation techies. 
+        <div className="p-10 lg:w-full"><div className='text-xl font-bold pb-2'>MentorMeCollective</div> MentorMeCollective is a non-profit organization focused on providing opportunites for first generation techies. 
          They recently partnered with Google to provide scholarships for their members to participate in Google's Grow With Me Program.
             The goal was for students to create a solution for the United Nations program. 
          I served as the UX Designer for the
@@ -17,11 +19,15 @@ export default function UxPage() {
           to educate and provide solutions for users from all demographics,
           races, and backgrounds on how they can reduce their risk for heart
           disease and decrease the heart mortality rates in the United States.
-          <div className='text-center bg-black px-10 py-5 text-white font-bold rounded-full mt-5'>
+          <div className='text-center bg-black px-10 py-5 text-white font-bold rounded-full mt-10'>
             <button>View Project</button>
             </div>
           </div>
-        <div className="p-10 lg:w-6/12"><strong>Image</strong></div>
+        <div className="p-10 lg:w-full flex">
+        <img src={hhg} alt="hhg-mobile-app" className="w-1/2" />
+        <img src={hhg2} alt="hhg-mobile-app2" className="w-1/2" />
+
+        </div>
       </section>
      
 
