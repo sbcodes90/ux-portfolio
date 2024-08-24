c814edf6ed4c49b2a239d792f3edf628892426cd
sbcodes90 <sydneycburrell29@gmail.com>
2024-08-23 22:03:56 -0400

    updates and re-organizatiom

--- my-app/src/components/GoogleProjectPage.js
+++ my-app/src/components/GoogleProjectPage.js
@@ -9,7 +9,7 @@ import sitemap from "../images/sitemap.png";
 import wireframes from "../images/Wireframes.png";
 import prototype from "../images/hhg-prototype.png";
 import { Link } from "react-router-dom";
-
+import userJourney from "../images/hhg-user-journey.png";
 export default function GoogleProjectPage() {
   return (
     <>
@@ -63,6 +63,10 @@ export default function GoogleProjectPage() {
             websites and apps to educate users, but who is offering solutions?
             How can our app stand out?
           </div>
+          <section>
+      <div className="font-semibold text-2xl pt-10">Logo Design</div>
+    <img src={logo} alt="hhg-logo" />
+    </section>
     <div className="font-semibold text-2xl pt-10">Empathize: Learning about our users</div>
     <div className="font-light text-xl pt-3">User Interviews</div>
     <div className="pb-5 font-light pt-2">
@@ -81,23 +85,9 @@ export default function GoogleProjectPage() {
       <UserProfile />
       <section>
       <div className="font-semibold text-2xl pt-10">SiteMap / Architecture</div>
-    <div className="font-light text-xl pt-3">User Interviews</div>
     <img src={sitemap} alt="sitemap" className="my-10" />
     </section>
-    <section>
-      <div className="font-semibold text-2xl pt-10">User Journey</div>
-    <div className="font-light text-xl pt-3">User Interviews</div>
-    <div className="pb-5 font-light pt-2">
-      Some common questions to begin a UX competitive analysis are: Who is
-      currently trying to solve this problem? How are they trying to solve
-      the problem? What their main differentiator or unique value-add is
-      for their business and products Did anyone try to solve it in the
-      past and fail? Why did they fail? Heart Disease in America is out of
-      control. There are many initiatives, and organizations that offer
-      websites and apps to educate users, but who is offering solutions?
-      How can our app stand out?
-    </div>
-    </section>
+    
     <section>
       <div className="font-semibold text-2xl pt-5">Wireframes</div>
       <div className="pb-5 font-light pt-2">
@@ -110,7 +100,10 @@ export default function GoogleProjectPage() {
       websites and apps to educate users, but who is offering solutions?
       How can our app stand out?
     </div>
-      <section>
+    
+    <div className="font-light text-xl pt-3 pb-5">First Iteration</div>
+  <img src={wireframes} alt="wireframes" />
+  <section>
       <div className="font-light text-xl pt-3">User Testing / Ideation Phase</div>
 {/*     <div className="font-light text-xl pt-3">User Interviews</div>
  */}    <div className="pb-5 font-light pt-2">
@@ -124,16 +117,14 @@ export default function GoogleProjectPage() {
       How can our app stand out?
     </div>
     </section>
-    <div className="font-light text-xl pt-3">First Iteration</div>
-  <img src={wireframes} alt="wireframes" />
-  <div className="font-light text-xl pt-3">Second Iteration</div>
+  <section>
+      <div className="font-light text-xl pt-10">User Journey</div>
+      <div className="font-light pt-3">To ensure a seamless user experience, it was crucial to empathetically observe the user journey, capture their emotions and feelings while using the app, and implement the user feedback to effectively help them achieve their goals.</div>
+      <img src={userJourney} alt="user-journey" className="rounded-3xl my-6" />
+    </section>
+  <div className="font-light text-xl pt-3 pb-5">Second Iteration</div>
   <img src={wireframes} alt="wireframes" />
 
-    </section>
- 
-    <section>
-      <div className="font-semibold text-2xl pt-10">Logo Design</div>
-    <img src={logo} alt="hhg-logo" />
     </section>
     <section>
       <div className="font-semibold text-2xl pt-10">Prototypes</div>
@@ -143,10 +134,10 @@ export default function GoogleProjectPage() {
    
         </div>
    
-    <img src={prototype}  alt="mobile-prototype" />
+    <img src={prototype} className="bg-gray-50 lg:p-20 rounded-3xl"  alt="mobile-prototype" />
     <div className="flex justify-center items-center p-20">
     <button className="bg-black text-white px-10 py-3 rounded-3xl ml-5">
-      <Link target="_blank" className="text-xl" to="https://www.figma.com/design/36Yy6UuTzh1H36rXo7Ua8w/HeartHealthGlobal-Design-Files?node-id=7-5&t=n2xehB5eQlQ1hU28-1" >Go to Figma</Link>
+      <Link target="_blank" className="text-lg" to="https://www.figma.com/design/36Yy6UuTzh1H36rXo7Ua8w/HeartHealthGlobal-Design-Files?node-id=7-5&t=n2xehB5eQlQ1hU28-1" >Go to Figma</Link>
         </button>
         </div>
      </section>
